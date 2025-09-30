cfed03810d421fae415c5499034563d16fa8f285
minadblue-cmyk <minadblue@gmail.com>
2025-09-30 11:50:01 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: './',
+  base: command === 'build' ? '/area-do-cliente/' : '/',
   build: {
-    assetsDir: 'assets'
-  }
-})
+    assetsDir: 'assets',
+  },
+}))
